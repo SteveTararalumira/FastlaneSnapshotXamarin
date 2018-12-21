494e129f7184f95438da509a3e5d09bb19497988
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-12-21 17:40:30 +0200

    Add FastlaneSnapshotXamarinTest project

---
+++ FastlaneSnapshotXamarinTest/FastlaneSnapshotXamarinTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneSnapshotXamarinTest
+//
+//  Created by Bohdan Hrybach on 12/21/18.
+//  Copyright © 2018 Bohdan Hrybach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

7ed0d5043c260d3de817376cedf05d219cc93b8d
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-12-21 16:53:48 +0200

    Add FastlaneSnapshotXamarin project

---
+++ FastlaneSnapshotXamarin/FastlaneSnapshotXamarin/ViewController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using UIKit;
+using System.Linq;
+
+namespace FastlaneSnapshotXamarin
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr intPtr) : base(intPtr)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            ReverseButton.TouchUpInside += ReverseButton_TouchUpInside;
+        }
+
+        public override void ViewDidUnload()
+        {
+            base.ViewDidUnload();
+            ReverseButton.TouchUpInside += ReverseButton_TouchUpInside;
+        }
+
+        private void ReverseButton_TouchUpInside(object sender, EventArgs e)
+        {
+            var originalText = ReverseTextField.Text;
+            var reversedText = Reverse(originalText);
+
+            ShowDialog(originalText, reversedText);
+        }
+
+        private String Reverse(String source)
+        {
+            var result = new String(source.Reverse().ToArray());
+            return result;
+        }
+
+        private void ShowDialog(String originalString, String reversedString)
+        {
+            var alertView = CreateAlertView(originalString, reversedString);
+            alertView.Show();
+        }
+
+        private UIAlertView CreateAlertView(String originalString, String reversedString)
+        {
+            var alertView = new UIAlertView();
+            alertView.Title = "Result";
+            alertView.Message = $"Original string: {originalString} \n Reversed string: {reversedString}";
+            alertView.AddButton("OK");
+
+            return alertView;
+        }
+    }
+}
---
+++ FastlaneSnapshotXamarin/FastlaneSnapshotXamarin/ViewController.designer.cs
@@ -0,0 +1,37 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace FastlaneSnapshotXamarin
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ReverseButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField ReverseTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ReverseButton != null) {
+                ReverseButton.Dispose ();
+                ReverseButton = null;
+            }
+
+            if (ReverseTextField != null) {
+                ReverseTextField.Dispose ();
+                ReverseTextField = null;
+            }
+        }
+    }
+}
